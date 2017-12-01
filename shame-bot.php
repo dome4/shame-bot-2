1ecaeca51a6c98733b537bac1e67119ea986f8f2
dome4 <dominic.seitz@gmx.de>
2017-12-02 00:42:30 +0100

    shame bot 1.0

--- shame-bot.php
+++ shame-bot.php
@@ -3,26 +3,54 @@ require __DIR__ . "/vendor/autoload.php";
 
 use React\EventLoop;
 use Slack\RealTimeClient;
+use Slack\Message\Attachment;
+use Slack\Channel;
 
 // include config file
 $config = include('config.php');
 
-
-
 $loop = EventLoop\Factory::create();
 
+// Slack RTM API
 $rtmClient = new RealTimeClient($loop);
 $rtmClient->setToken($config['slack-token']);
 
-// disconnect after first message
-$rtmClient->on('message', function ($data) use ($rtmClient) {
-    echo "Someone typed a message: ".$data['text']."\n";
-    $rtmClient->disconnect();
+// Slack Web API
+$webAPIClient = new \Slack\ApiClient($loop);
+$webAPIClient->setToken($config['slack-token']);
+
+
+$rtmClient->on('message', function ($recMessage) use ($rtmClient, $webAPIClient) {
+
+    // handle every message which has more than 10 characters and reply on the same channel
+    if(strlen($recMessage['text']) >= 10 && $recMessage['bot_id'] == ''){
+
+
+        $webAPIClient->getChannelById($recMessage['channel'])->then(function (Channel $channel) use ($webAPIClient, $recMessage) {
+
+            // attachement
+            $data = [
+                'fallback' => 'Shame on you!',
+                'color' => '#FF0000',
+                'image_url' => 'https://pbs.twimg.com/media/Cqu5MhRWcAEz0Wk.jpg'
+            ];
+
+            // new message
+            $newMessage = $webAPIClient->getMessageBuilder()
+                ->setText('Shame on you, <@' .$recMessage['user']. '>!')
+                ->setChannel($channel)
+                ->addAttachment(Attachment::fromData($data))
+                ->create();
+
+            // send response
+            $webAPIClient->postMessage($newMessage);
+        });
+    }
 });
 
 // Connect to the Slack server.
 $rtmClient->connect()->then(function () {
-    echo "Connected!\n";
+    echo "ShameBot connected!\n";
 });
 
 
